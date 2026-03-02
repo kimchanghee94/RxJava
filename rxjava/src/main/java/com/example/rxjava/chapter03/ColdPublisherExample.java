9aba6564f32764dceccadef2873cecc3265d19b1
kimchanghee <kimchanghee94@naver.com>
2026-03-02 20:39:28 +0900

    #main 1. chapter03 => Reactive Hot & Cold 차이

---
+++ rxjava/src/main/java/com/example/rxjava/chapter03/ColdPublisherExample.java
@@ -0,0 +1,11 @@
+package com.example.rxjava.chapter03;
+
+import io.reactivex.rxjava3.core.Flowable;
+
+public class ColdPublisherExample {
+    public static void main(String[] args){
+        Flowable<Integer> flowable = Flowable.just(1,3,5,7);
+        flowable.subscribe(data->System.out.println("구독자1 : " + data));
+        flowable.subscribe(data->System.out.println("구독자2 : " + data));
+    }
+}
